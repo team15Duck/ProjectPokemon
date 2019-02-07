83cf4c73622626b1c9765c42f9bb574af8d9ba21
fairychoi <ccw5763@gmail.com>
2019-02-07 12:10:27 +0900

    ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

---
+++ team15duckD2D/playerDataBase.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "playerDataBase.h"
+
+
+playerDataBase::playerDataBase()
+{
+}
+
+
+playerDataBase::~playerDataBase()
+{
+}
---
+++ team15duckD2D/playerDataBase.h
@@ -0,0 +1,8 @@
+#pragma once
+class playerDataBase
+{
+public:
+	playerDataBase();
+	~playerDataBase();
+};
+
