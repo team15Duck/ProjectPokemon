77a567a2e3b3884f1ea993694c4b0c7c2fb009c9
fairychoi <ccw5763@gmail.com>
2019-02-07 14:17:31 +0900

    ㅇㅇ

--- team15duckD2D/battleScene.h
+++ team15duckD2D/battleScene.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "battle.h"
-#include <list>
+#include <set>
 #include "gameNode.h"
 //포켓몬과배틀
 class battleScene : public gameNode
@@ -9,8 +9,7 @@ private:
 
 	BattlePhase _phase;			//배틀 단계 정보			
 	BattleTurn _turn;			//턴
-	UINT _exp[6];				//경험치 분배 전 총 경험치 저장
-	list<UINT> _expList;		//경험치 받을 포켓몬 리스트 
+	set<UINT> _expList;			//경험치 받을 포켓몬 리스트 
 								//적이 죽을때마다 클리어되고 현재 포켓몬이 추가됨.
 
 public:
