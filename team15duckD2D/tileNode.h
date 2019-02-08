936762fcf913cd381cf42e0c17eddf989a854695
byeolchic <46617120+byeolchic@users.noreply.github.com>
2019-02-08 12:03:33 +0900

    재원 타일노드 define만 좀함

--- team15duckD2D/tileNode.h
+++ team15duckD2D/tileNode.h
@@ -1,23 +1,26 @@
 #pragma once
 #include "stdafx.h"
 
-
 //요거 속성별로 다 추가해주세요 이거 있으면 판단이 너무쉬워집니다
 //2배로 더해주세요
 #define TILE_SIZE	64
 
 #define ATTR_NONE   0x00000000
 #define ATTR_UNMOVE 0x00000001
+#define ATTR_WALL	0X00000016
 #define ATTR_WATER  0x00000002
 #define ATTR_FOREST 0x00000004
 #define ATTR_TREE   0x00000008
-#define ATTR_WALL	0X00000016
 #define ATTR_ROCK	0X00000032
 #define ATTR_HILL	0X00000064
 
-
 ///////// 어트리뷰트는 처음에 0으로 초기화하고 |= 이걸로 더해주세요
 ///////// 빼낼떄는 ^=
+
+
+
+
+
 typedef struct tagTile
 {
 	UINT terrainFrameX;		//이거는 -1로 초기화 합시다  -1일때 출력안하게요
