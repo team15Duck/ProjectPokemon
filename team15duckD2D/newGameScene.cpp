6416ccff6b678fb28e4b43a67304c26d0e36509f
fairychoi <ccw5763@gmail.com>
2019-02-11 10:50:27 +0900

    dddd

---
+++ team15duckD2D/newGameScene.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "newGameScene.h"
+
+
+newGameScene::newGameScene()
+{
+}
+
+
+newGameScene::~newGameScene()
+{
+}
---
+++ team15duckD2D/newGameScene.h
@@ -0,0 +1,8 @@
+#pragma once
+class newGameScene
+{
+public:
+	newGameScene();
+	~newGameScene();
+};
+
