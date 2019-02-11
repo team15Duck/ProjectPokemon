36027b2cc6322fc8b0bd61ef33373380aadd790b
fairychoi <ccw5763@gmail.com>
2019-02-11 11:10:45 +0900

    dddddddddddddddddd

--- team15duckD2D/IllustratedBook.cpp
+++ team15duckD2D/IllustratedBook.cpp
@@ -10,3 +10,29 @@ IllustratedBook::IllustratedBook()
 IllustratedBook::~IllustratedBook()
 {
 }
+
+HRESULT IllustratedBook::init()
+{
+	for (int i = 0; i < 151; i++)
+	{
+		_book[i].name = "???";
+		_book[i].attr = "???";
+		_book[i].weight = "???";
+		_book[i].height = "???";
+	}
+
+
+	return S_OK;
+}
+
+void IllustratedBook::release()
+{
+}
+
+void IllustratedBook::update()
+{
+}
+
+void IllustratedBook::render()
+{
+}
--- team15duckD2D/IllustratedBook.h
+++ team15duckD2D/IllustratedBook.h
@@ -1,8 +1,34 @@
 #pragma once
+
+
+typedef struct tagPokemonBookInfo
+{
+	string name;	//이름
+	string attr;	//속성
+	string weight;	//무게
+	string height;	//키
+}POKEMONBOOK;
+
+
 class IllustratedBook
 {
+private:
+
+	POKEMONBOOK _book[151];
+
+
+
 public:
 	IllustratedBook();
 	~IllustratedBook();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	POKEMONBOOK* getBook() { return _book; }
+
+
 };
 
