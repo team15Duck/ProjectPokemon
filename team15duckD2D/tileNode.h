c52ead330f82cc4dd10b73d1b991e0cf22825e1c
byeolchic <46617120+byeolchic@users.noreply.github.com>
2019-02-13 09:12:46 +0900

    tileNode 수정한거 올릴게여(string수정)

--- team15duckD2D/tileNode.h
+++ team15duckD2D/tileNode.h
@@ -40,65 +40,91 @@
 
 
 //타일 이미지 이름
-static string TERRAIN_NAME_NONE = "";
-static string TERRAIN_NAME1		= "tile_01";
-static string TERRAIN_NAME2		= "tile_02";
-static string TERRAIN_NAME3		= "tile_03";
-static string TERRAIN_NAME4		= "tile_04";
-static string TERRAIN_NAME5		= "tile_05";
-static string TERRAIN_NAME6		= "tile_06";
-static string TERRAIN_NAME7		= "tile_07";
-static string TERRAIN_NAME8		= "tile_08";
-static string TERRAIN_NAME9		= "tile_09";
-static string TERRAIN_NAME10	= "tile_10";
-static string TERRAIN_NAME11	= "tile_11";
-
-
-//enum TERRAIN_NAME_INFO
-//{
-//	TERRAIN_NAME_NONE,
-//	TERRAIN_NAME1	 ,
-//	TERRAIN_NAME2	 ,
-//	TERRAIN_NAME3	 ,
-//	TERRAIN_NAME4	 ,
-//	TERRAIN_NAME5	 ,
-//	TERRAIN_NAME6	 ,
-//	TERRAIN_NAME7	 ,
-//	TERRAIN_NAME8	 ,
-//	TERRAIN_NAME9	 ,
-//	TERRAIN_NAME10	 ,
-//	TERRAIN_NAME_COUNT
-//};
-//
-//static string TERRAIN_NAME[TERRAIN_NAME_COUNT] = { "","tile_01" ,"tile_02" ,"tile_03" ,"tile_04" ,"tile_05" ,"tile_06" ,"tile_07" ,"tile_08" ,"tile_09", "tile_10" };
+//static string TERRAIN_NAME_NONE = "";
+//static string TERRAIN_NAME1		= "tile_01";
+//static string TERRAIN_NAME2		= "tile_02";
+//static string TERRAIN_NAME3		= "tile_03";
+//static string TERRAIN_NAME4		= "tile_04";
+//static string TERRAIN_NAME5		= "tile_05";
+//static string TERRAIN_NAME6		= "tile_06";
+//static string TERRAIN_NAME7		= "tile_07";
+//static string TERRAIN_NAME8		= "tile_08";
+//static string TERRAIN_NAME9		= "tile_09";
+//static string TERRAIN_NAME10	= "tile_10";
+//static string TERRAIN_NAME11	= "tile_11";
 
 //오브젝트 이미지 이름
-static string OBJECT_NAME_NONE	= "";
-static string OBJECT_NAME1		= "object_01";
-static string OBJECT_NAME2		= "object_02";
-static string OBJECT_NAME3		= "object_03";
-static string OBJECT_NAME4		= "object_04";
-static string OBJECT_NAME5		= "object_05";
-static string OBJECT_NAME6		= "object_06";
-static string OBJECT_NAME7		= "object_07";
-static string OBJECT_NAME8		= "object_08";
-static string OBJECT_NAME9		= "object_09";
-static string OBJECT_NAME10		= "object_10";
-static string OBJECT_NAME11		= "object_11";
-static string OBJECT_NAME12		= "object_12";
-static string OBJECT_NAME13		= "object_13";
-static string OBJECT_NAME14		= "object_14";
-static string OBJECT_NAME15		= "object_15";
-static string OBJECT_NAME16		= "object_16";
-static string OBJECT_NAME17		= "object_17";
-static string OBJECT_NAME18		= "object_18";
+//static string OBJECT_NAME_NONE	= "";
+//static string OBJECT_NAME1		= "object_01";
+//static string OBJECT_NAME2		= "object_02";
+//static string OBJECT_NAME3		= "object_03";
+//static string OBJECT_NAME4		= "object_04";
+//static string OBJECT_NAME5		= "object_05";
+//static string OBJECT_NAME6		= "object_06";
+//static string OBJECT_NAME7		= "object_07";
+//static string OBJECT_NAME8		= "object_08";
+//static string OBJECT_NAME9		= "object_09";
+//static string OBJECT_NAME10		= "object_10";
+//static string OBJECT_NAME11		= "object_11";
+//static string OBJECT_NAME12		= "object_12";
+//static string OBJECT_NAME13		= "object_13";
+//static string OBJECT_NAME14		= "object_14";
+//static string OBJECT_NAME15		= "object_15";
+//static string OBJECT_NAME16		= "object_16";
+//static string OBJECT_NAME17		= "object_17";
+//static string OBJECT_NAME18		= "object_18";
+
+enum TERRAIN_NAME_INFO
+{
+	TERRAIN_NAME_NONE,
+	TERRAIN_NAME1	 ,
+	TERRAIN_NAME2	 ,
+	TERRAIN_NAME3	 ,
+	TERRAIN_NAME4	 ,
+	TERRAIN_NAME5	 ,
+	TERRAIN_NAME6	 ,
+	TERRAIN_NAME7	 ,
+	TERRAIN_NAME8	 ,
+	TERRAIN_NAME9	 ,
+	TERRAIN_NAME10	 ,
+	TERRAIN_NAME11	 ,
+
+	TERRAIN_NAME_COUNT
+};
+enum OBJECT_NAME_INFO
+{
+	OBJECT_NAME_NONE,
+	OBJECT_NAME1	,
+	OBJECT_NAME2	,
+	OBJECT_NAME3	,
+	OBJECT_NAME4	,
+	OBJECT_NAME5	,
+	OBJECT_NAME6	,
+	OBJECT_NAME7	,
+	OBJECT_NAME8	,
+	OBJECT_NAME9	,
+	OBJECT_NAME10	,
+	OBJECT_NAME11	,
+	OBJECT_NAME12	,
+	OBJECT_NAME13	,
+	OBJECT_NAME14	,
+	OBJECT_NAME15	,
+	OBJECT_NAME16	,
+	OBJECT_NAME17	,
+	OBJECT_NAME18	,
+	OBJECT_NAME_COUNT
+};
+
+static string TERRAIN_NAME[TERRAIN_NAME_COUNT] = { "","tile_01" ,"tile_02" ,"tile_03" ,"tile_04" ,"tile_05" ,"tile_06" ,"tile_07" ,"tile_08" ,"tile_09", "tile_10", "tile_11" };
+static string OBJECT_NAME[TERRAIN_NAME_COUNT] = { "","tile_01" ,"tile_02" ,"tile_03" ,"tile_04" ,"tile_05" ,"tile_06" ,"tile_07" ,"tile_08" ,"tile_09", "tile_10" };
+
 
 
 typedef class tagTile
 {
 public:
-	tagTile()  :terrainImageName	("tt"),
-				objectImageName		("tt"),
+	tagTile()  :terrainImageIndex	(NULL),
+				objectImageIndex	(NULL),
 				terrainFrameX		(NULL),
 				terrainFrameY		(NULL),
 				objectFrameX		(NULL),
@@ -109,28 +135,26 @@ public:
 	{};
 
 	//TERRAIN_NAME_INFO terrainImageName;	//TERRAIN_NAME_NONE 으로 초기화
-	string	terrainImageName;
+	UINT	terrainImageIndex;
 	UINT	terrainFrameX;			//0으로 초기화
 	UINT	terrainFrameY;			//0으로 초기화
-	string  objectImageName;		//OBJECT_NAME_NONE 으로 초기화
+	UINT	objectImageIndex;		//OBJECT_NAME_NONE 으로 초기화
 	UINT	objectFrameX;			//0으로 초기화
 	UINT	objectFrameY;			//0으로 초기화
 	DWORD	attr;					//이건 ATTR_NONE으로 초기화 후에 시작합시다
 
 	void setTile(tagTile tile)
 	{
-		if(tile.terrainImageName.size() > 0)
-			terrainImageName = tile.terrainImageName;
+		if(tile.terrainImageIndex > 0)
+			terrainImageIndex = tile.terrainImageIndex;
 		terrainFrameX = tile.terrainFrameX;
 		terrainFrameY = tile.terrainFrameY;
-		if (tile.objectImageName.size() > 0)
-			objectImageName = tile.objectImageName;
+		if (tile.objectImageIndex > 0)
+			objectImageIndex = tile.objectImageIndex;
 		objectFrameX = tile.objectFrameX;
 		objectFrameY = tile.objectFrameY;
 		attr = tile.attr;
 	}
-
-
 
 }TILE;
 
