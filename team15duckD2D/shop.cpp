3b89efbaf6846f470c765334083caead539f0c65
921126 <nabom921126@gmail.com>
2019-02-15 22:09:42 +0900

    ddd
    dddd

---
+++ team15duckD2D/shop.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "shop.h"
+
+
+shop::shop()
+{
+}
+
+
+shop::~shop()
+{
+}
+
+HRESULT shop::init()
+{
+	return S_OK;
+}
+
+void shop::release()
+{
+}
+
+void shop::update()
+{
+}
+
+void shop::render()
+{
+}
---
+++ team15duckD2D/shop.h
@@ -0,0 +1,16 @@
+#pragma once
+class shop
+{
+private:
+
+public:
+	shop();
+	~shop();
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+};
+
