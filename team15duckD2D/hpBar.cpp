01b312830b003abbaae999ad701252a82c02b035
921126 <nabom921126@gmail.com>
2019-02-19 22:58:42 +0900

    gpgpgpg
    ggg

--- team15duckD2D/hpBar.cpp
+++ team15duckD2D/hpBar.cpp
@@ -10,3 +10,36 @@ hpBar::hpBar()
 hpBar::~hpBar()
 {
 }
+
+
+HRESULT hpBar::init(string imgname, int framX, int framY, int x, int y, int width, int height, bool isbottomuse)
+{
+	_x = x;
+	_y = y;
+
+	_hpBar = { _x, _y, width, height };
+
+	_maxwidht = width;
+	_height = height;
+
+	_isbottomuse = isbottomuse;
+	return S_OK;
+}
+
+void hpBar::release()
+{
+}
+
+void hpBar::update()
+{
+}
+
+void hpBar::render()
+{
+}
+
+
+void hpBar::setHPbar(float currentHP, float maxHP)
+{
+	_width = (currentHP / maxHP) * _maxwidht;
+}
--- team15duckD2D/hpBar.h
+++ team15duckD2D/hpBar.h
@@ -1,8 +1,32 @@
 #pragma once
-class hpBar
+#include "gameNode.h"
+
+class hpBar : public gameNode
 {
+private:
+	
+	D2D1_RECT_F _hpBar;
+
+	float _x;			//좌표
+	float _y;			//크기
+	float _width;		//가로크기
+	float _maxwidht;	//최대가로크기
+	float _height;		//세로크기
+
+	string _imgName;
+	bool _isbottomuse;
 public:
 	hpBar();
 	~hpBar();
+
+	HRESULT init(string imgname, int framX, int framY, int x, int y, int width, int height, bool isbottomuse);
+	void release();
+	void update();
+	void render();
+
+	void setHPbar(float currentHP, float maxHP);
+
+	void setX(int x) { _x = x; }
+	void setY(int y) { _y = y; }
 };
 
